d2ee7b0f65c47ab254ba3ebb4ae0c90ee8faf4e8
sohamgarg2020 <sohamgarg2007@gmail.com>
2021-01-21 13:42:41 -0800

    Finished

---
+++ vignere_cipher.py
@@ -0,0 +1,16 @@
+alpha_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+def vignere(text, number):
+    char_letter = ""
+    for word in text.split():
+        for char in word:
+            if not(char.isalpha()):
+                return f"Make sure you only use letters when you want to encrypt"
+            char_num = int(alpha_list.index(char.lower()))
+            new_char_num = int(char_num + int(number))
+            new_char_num %= 26
+            char_letter += str(alpha_list[int(new_char_num)])
+        char_letter += " "
+    return f"Encrypted text is: {char_letter.rstrip()}"
+
+print(vignere("text", "5"))
